406b3a9e1a10637816ce389be1cff1ee8a81d95f
Sean Collins <sean.collins@outlook.com>
2018-09-23 18:07:59 -0700

    Add yelp-fusion to the project

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const clientID = 'w1AnwjlOriaMYXqCtU3izw'
+const apiKey = 'eaQbNnUvFPMlBONpRWDphtKNHf_Ai-Clz6FmY8Gnwlgg0hyul9ZLjWQZhdJbhIs2Yb9hKSguerHXfC7BEuWVKkF8GPDnQNfuiB16bkr18pnQZg0B6ULNXEVvwimoW3Yx'
+
 app.prepare()
   .then(() => {
     const server = express()
